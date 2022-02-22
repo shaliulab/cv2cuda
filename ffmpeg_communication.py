a8d7a386941bcc7a400c3e14e78a9f7ee0895d7a
antortjim <ntoniohu@gmail.com>
2022-02-22 16:45:36 +0000

    Update comm module

--- ffmpeg_communication.py
+++ ffmpeg_communication.py
@@ -3,10 +3,8 @@ import logging
 import shlex
 
 PIX_FMT = "rgb24"
-# PIX_FMT = "yuv420p"
 
-
-def init_ffmpeg(cmd, *args, action: str = "", **kwargs):
+def init_ffmpeg(cmd):
     ffmpeg_cmd = subprocess.Popen(
         cmd,
         stdin=subprocess.PIPE,
@@ -16,19 +14,15 @@ def init_ffmpeg(cmd, *args, action: str = "", **kwargs):
     return ffmpeg_cmd
 
 
-def write_to_ffmpeg(ffmpeg_cmd, input_bytes: bytes) -> bytes or None:
-    b = b''
+def write_to_ffmpeg(ffmpeg_cmd, data: bytes) -> bytes or None:
     # write bytes to processe's stdin and close the pipe to pass
     # data to piped process
     # check if stdin is open
-    logging.debug(f"Writing {len(input_bytes)} bytes...")
-    ffmpeg_cmd.stdin.write(input_bytes)
-    logging.debug(f"Writing {len(input_bytes)} bytes done")
-    return b
+    logging.debug(f"Writing {len(data)} bytes...")
+    ffmpeg_cmd.stdin.write(data)
+    logging.debug(f"Writing {len(data)} bytes done")
 
 def make_command(device, width, height, fps, output):
-
-    # -rc-lookahead N 
 
     if device == "cpu":
 
@@ -36,9 +30,7 @@ def make_command(device, width, height, fps, output):
             f" -s {width}x{height} -r {str(fps)}"\
             f" -i - -an -vcodec mpeg4 {output}"
             
-        # -pix_fmt yuv420p -preset slow -rc vbr_hq -b:v 8M -maxrate:v 10M -c:a aac -b:a 224k"\
     elif device == "gpu":
-        # -hwaccel_output_format cuda -hwaccel cuvid"\
         command = f"ffmpeg -y -f rawvideo -pix_fmt {PIX_FMT}"\
             " -vsync 0 -hwaccel cuda"\
             f" -s {width}x{height} -r {str(fps)}"\
@@ -47,6 +39,5 @@ def make_command(device, width, height, fps, output):
 
     print(command)
     command = shlex.split(command)
-    
     return command
 
