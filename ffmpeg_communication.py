d78195dd94f6f01c5b0a76394f1ffbe1dade3ed0
antortjim <ntoniohu@gmail.com>
2022-02-22 16:47:07 +0000

    Update comm module

--- ffmpeg_communication.py
+++ ffmpeg_communication.py
@@ -4,6 +4,7 @@ import shlex
 
 PIX_FMT = "rgb24"
 
+
 def init_ffmpeg(cmd):
     ffmpeg_cmd = subprocess.Popen(
         cmd,
@@ -15,12 +16,10 @@ def init_ffmpeg(cmd):
 
 
 def write_to_ffmpeg(ffmpeg_cmd, data: bytes) -> bytes or None:
-    # write bytes to processe's stdin and close the pipe to pass
-    # data to piped process
-    # check if stdin is open
     logging.debug(f"Writing {len(data)} bytes...")
     ffmpeg_cmd.stdin.write(data)
     logging.debug(f"Writing {len(data)} bytes done")
+
 
 def make_command(device, width, height, fps, output):
 
@@ -33,9 +32,12 @@ def make_command(device, width, height, fps, output):
     elif device == "gpu":
         command = f"ffmpeg -y -f rawvideo -pix_fmt {PIX_FMT}"\
             " -vsync 0 -hwaccel cuda"\
-            f" -s {width}x{height} -r {str(fps)}"\
-            f" -i - -an -c:v h264_nvenc {output}"
-            # f" -i - -an -c:v h264_nvenc -f null - "#{output}"
+            f" -s {width}x{height} -r {str(fps)}"
+        
+        # if you want to write to disk
+        command += f" -i - -an -c:v h264_nvenc {output}"
+        # if you dont want to write to disk
+        # command += f" -i - -an -c:v h264_nvenc -f null - "#{output}"
 
     print(command)
     command = shlex.split(command)
